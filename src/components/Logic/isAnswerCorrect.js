ac28623b560c20bab66d55f6b5dfa9288638a97b
Hanna Kozak <hannawkozak@gmail.com>
2021-12-07 22:43:29 +0000

    SWQ-13-Verifying-the-correctness-of-the-answer

---
+++ src/components/Logic/isAnswerCorrect.js
@@ -0,0 +1,11 @@
+export default function isAnswerCorrect(correctAnswer, playerAnswer){
+  const isCorrectAnswerString = typeof correctAnswer === 'string';
+  const isPlayerAnswerString = typeof playerAnswer === 'string';
+    if (!isCorrectAnswerString){
+      throw new Error('correctAnswer has to be a string')
+    }
+    if (!isPlayerAnswerString){
+      throw new Error('playerAnswer has to be a string')
+    }
+    return correctAnswer.toLowerCase() === playerAnswer.toLowerCase()
+}
