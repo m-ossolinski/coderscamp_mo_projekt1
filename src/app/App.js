b740a76ce44076c4c1b5bb84689857b5db70f677
Magdalena Kostrzewa <magdalena.kostrzewa@coderscrew.pl>
2021-12-09 20:45:15 +0100

    SWQ-21-Star-Wars-logo

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,6 @@
+import createLogo from '../components/swLogo/swLogo';
+
 export const App = ({ options }) => {
-    console.log('test');
-}
-
-
+  console.log('test');
+  document.getElementById('swquiz-app').appendChild(createLogo()); //logo to be placed in final layout
+};
