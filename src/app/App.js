4f5101239ac1bc3b61b6054a9494680d605f2cd4
Magdalena Piotrowska <magdalena25piotrowska@gmail.com>
2021-12-10 21:49:14 +0100

    code-review

--- src/app/App.js
+++ src/app/App.js
@@ -1,20 +1,4 @@
-import Player from "../services/player/player";
-import Rank from "../services/rank/rank";
-
 
 export const App = ({ options }) => {
-
-    let player = new Player("Gracz10", 10);
-    player.save();
-
-    const currentMode = 'vehicles';
-
-    const rank = new Rank();
-
-    rank.addToRank(currentMode, player);
-
-    rank.getRank()[currentMode].map((rankItem, idx) => {
-        console.log('miejsce', idx + 1, 'nazwa', rankItem.nickName)
-    })
 
 }
--- src/services/player/player.js
+++ src/services/player/player.js
@@ -1,12 +1,12 @@
 export default class Player {
-    constructor(nickName, points) {
-        this.nickName = nickName;
+    constructor(playerName, points) {
+        this.playerName = playerName;
         this.points = points;
         this.id = Date.now();
     }
 
     save() {
-        localStorage.setItem('nickName', this.nickName);
+        localStorage.setItem('playerName', this.playerName);
         localStorage.setItem('points', this.points);
     }
 
