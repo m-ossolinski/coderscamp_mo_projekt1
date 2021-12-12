2a078694e5dca2f5c10db117e7751a271b5fa82d
justynadabrowska <74719040+justynadabrowska@users.noreply.github.com>
2021-12-12 20:21:30 +0000

    created game mode component with styles, structure and simple logic

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,14 @@
 import createLogo from '../components/swLogo/swLogo';
 import generateQuestionForTheGameMode from '../services/game/generateQuestions';
 import { createMainMenu } from '../components/mainMenu/mainMenu';
+import {
+  gameMode,
+  PEOPLE_MODE_QUESTION
+} from '../components/gameMode/gameMode';
 
 export const App = ({ options }) => {
   document.getElementById('swquiz-app').appendChild(createLogo()); //logo to be placed in final layout
   console.log(generateQuestionForTheGameMode('people'));
   document.getElementById('swquiz-app').appendChild(createMainMenu());
+  console.log(gameMode(PEOPLE_MODE_QUESTION));
 };
---
+++ src/components/gameMode/gameMode.js
@@ -0,0 +1,33 @@
+import './gameMode.css';
+import {
+  PEOPLE_MODE,
+  VEHICLES_MODE,
+  STARSHIPS_MODE
+} from '../mainMenu/mainMenu';
+
+export const PEOPLE_MODE_QUESTION = 'Who is this Character?';
+export const VEHICLES_MODE_QUESTION = 'Do you recognize this vehicle?';
+export const STARSHIPS_MODE_QUESTION = 'Do you recognize this starship?';
+
+export const gameMode = (modeQuestion) => {
+  const questionContainer = document.createElement('div');
+  const app = document.getElementById('swquiz-app');
+  app.appendChild(questionContainer);
+
+  questionContainer.id = 'question';
+  questionContainer.className = 'question';
+  questionContainer.setAttribute('data-id', 'question');
+  let questionText = document.createTextNode(`MODE: ${modeQuestion}`);
+  questionContainer.appendChild(questionText);
+};
+
+export const getGameModeQuestion = (gameMode) => {
+  switch (gameMode) {
+    case PEOPLE_MODE:
+      return PEOPLE_MODE_QUESTION;
+    case VEHICLES_MODE:
+      return VEHICLES_MODE_QUESTION;
+    case STARSHIPS_MODE:
+      return STARSHIPS_MODE_QUESTION;
+  }
+};
