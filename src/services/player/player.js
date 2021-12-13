f3d4a89b1aa8c76e3ea09562b71ab25a694fa946
Kos Krzysztof <krzysztof.kos33@gmail.com>
2021-12-13 16:40:16 +0100

    Update class Player, add function getPlayerHuman

--- src/services/player/player.js
+++ src/services/player/player.js
@@ -3,11 +3,28 @@ export default class Player {
         this.playerName = playerName;
         this.points = points;
         this.id = Date.now();
+        this.questions = 0;
+        this.answers = 0;
+        this.correctAnswers = 0;
     }
 
     save() {
         localStorage.setItem('playerName', this.playerName);
         localStorage.setItem('points', this.points);
+    }
+
+    askQuestion(callback) {
+        const isCallback = typeof callback == 'function';
+        if(!isCallback) throw new Error('Argument callback have to be a function');
+
+        callback();
+    }
+
+    giveAnswer(callback) {
+        const isCallback = typeof callback == 'function';
+        if(!isCallback) throw new Error('Argument callback have to be a function');
+
+        callback();
     }
 
 }
