46340c86e9025f58a5a9a809868d4f56e5091679
Justyna Dabrowska <74719040+justynadabrowska@users.noreply.github.com>
2021-12-14 17:23:19 +0000

    created game mode component with styles, structure and logic (#18)

--- src/app/App.js
+++ src/app/App.js
@@ -2,12 +2,17 @@ import createLogo from '../components/swLogo/swLogo';
 import generateQuestionForTheGameMode from '../services/game/generateQuestions';
 import { createMainMenu } from '../components/mainMenu/mainMenu';
 import createImgElementPeopleMode from '../components/recognitionImg/ImgModePeople/ImgModePeople';
+import {
+  gameMode,
+  PEOPLE_MODE_QUESTION
+} from '../components/gameMode/gameMode';
 
 export const App = async ({ options }) => {
   const swquiz = document.getElementById('swquiz-app');
 
   swquiz.appendChild(createLogo());
   swquiz.appendChild(createMainMenu());
+  swquiz.appendChild(gameMode(PEOPLE_MODE_QUESTION));
 
   const question = await generateQuestionForTheGameMode('people');
 
