307a31661a70dd651277bed79df5131bb42a43b5
Magdalena Kostrzewa <magdalena.kostrzewa@coderscrew.pl>
2021-12-14 19:06:14 +0100

    SWQ-22-Auto-player-random-answers

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,11 @@
 import createLogo from '../components/swLogo/swLogo';
 import generateQuestionForTheGameMode from '../services/game/generateQuestions';
 import { createMainMenu } from '../components/mainMenu/mainMenu';
+import getAutoPlayer from '../services/player/autoPlayer';
 
 export const App = ({ options }) => {
   document.getElementById('swquiz-app').appendChild(createLogo()); //logo to be placed in final layout
   console.log(generateQuestionForTheGameMode('people'));
   document.getElementById('swquiz-app').appendChild(createMainMenu());
+  console.log(getAutoPlayer().getAnswer('some image', ['a', 'b', 'c', 'd']));
 };
---
+++ src/services/player/autoPlayer.js
@@ -0,0 +1,36 @@
+// export default
+class AutoPlayer {
+  constructor() {
+    this.id = 'AutoPlayer';
+  }
+
+  generateAutoPlayerAnswer(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  getAnswer(image, answers) {
+    // console.log(image);
+    // console.log(answers);
+    if (typeof image !== 'string') {
+      throw new Error('image type error');
+    }
+    if (Array.isArray(answers) == false) {
+      throw new Error('answers is not an array error');
+    }
+    if (answers.length !== 4) {
+      throw new Error('expected 4 answers');
+    }
+    const autoPlayerAnswerIndex = this.generateAutoPlayerAnswer(0, 3);
+    // console.log(autoPlayerAnswerIndex);
+    const autoPlayerAnswer = answers[autoPlayerAnswerIndex];
+
+    return autoPlayerAnswer;
+  }
+}
+
+export default function getAutoPlayer() {
+  const autoPlayer = new AutoPlayer();
+  return autoPlayer;
+}
