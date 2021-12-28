e6ac21797d2fc46d314441994632d0678ea48f49
Kos Krzysztof <krzysztof.kos33@gmail.com>
2021-12-28 18:50:48 +0100

    Update component whiteButton

--- src/components/WhiteButton/WhiteButton.js
+++ src/components/WhiteButton/WhiteButton.js
@@ -1,28 +1,26 @@
 import './whiteButton.css';
 
 export default function whiteButton(iconSrc, text, callback) {
-    const btnWhite = document.createElement('button');
 
-    btnWhite.className = 'btnWhite';
+  if(typeof iconSrc !== 'string') throw new Error('An error occurred while creating white button: argument iconSrc have to be a string type');
+  if(typeof text !== 'string') throw new Error('An error occurred while creating white button: argument text have to be a string type');
+  if(typeof callback !== 'function') throw new Error('An error occurred while creating white button: argument callback have to be a function');
 
-    if (iconSrc) {
-        const icon = document.createElement('img');
-        icon.src = iconSrc;
-        icon.className = 'icon';
-        btnWhite.appendChild(icon);
-    }
-    if (text) {
-        text = text.charAt(0).toUpperCase() + text.slice(1);
-        const textElement = document.createElement('span');
-        textElement.innerHTML = text;
-        textElement.className = 'text';
-        btnWhite.appendChild(textElement);
-    }
+  const btnWhite = document.createElement('button');
+  btnWhite.className = 'btnWhite';
 
-    if (callback) {
-        btnWhite.onclick = callback;
-    }
+  const icon = document.createElement('img');
+  icon.src = iconSrc;
+  icon.className = 'icon';
+  btnWhite.appendChild(icon);
 
-    return btnWhite;
+  text = text.charAt(0).toUpperCase() + text.slice(1);
+  const textElement = document.createElement('span');
+  textElement.innerHTML = text;
+  textElement.className = 'text';
+  btnWhite.appendChild(textElement);
+  btnWhite.onclick = callback;
+
+  return btnWhite;
 
 }
