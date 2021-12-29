8c3008de32a92e01f516225ee40891f14bd9ad4b
Kos Krzysztof <krzysztof.kos33@gmail.com>
2021-12-29 18:08:38 +0100

    Added guiz game view

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,9 @@
+import { createMainView } from '../components/mainView/mainView';
+import { createQuizGameView } from '../components/quizGameView/quizGameView';
+import Game from '../services/game/game'
+import { GAME_MODES } from '../consts/GAME_MODES';
 import generateQuestionForTheGameMode from '../services/game/generateQuestions';
 import { getAutoPlayer } from '../services/player/autoPlayer';
-import { createMainView } from '../components/mainView/mainView';
 import createLogo from '../components/swLogo/swLogo';
 import { createMainMenu } from '../components/mainMenu/mainMenu';
 import createImgElementPeopleMode from '../components/recognitionImg/ImgModePeople/ImgModePeople';
@@ -12,10 +15,10 @@ import { createGameRulesComponent } from '../components/gameRules/gameRules';
 
 export const App = async ({ options }) => {
   const swquiz = document.getElementById('swquiz-app');
-  const question = await generateQuestionForTheGameMode('people');
-
+  const question = await generateQuestionForTheGameMode(GAME_MODES.people);
   const gameRules = {gameMode: 'people', answer: question.rightAnswer};
+  console.log(GAME_MODES.people);
 
   swquiz.appendChild(createMainView(question.image, gameRules));
-
+  swquiz.appendChild(createQuizGameView(question, GAME_MODES.people));
 };
