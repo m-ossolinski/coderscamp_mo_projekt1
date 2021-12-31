21083b2820f6e89fdfbfd7a24144d544d5e40aac
Agnieszka Urbanowicz <46648993+axseinga@users.noreply.github.com>
2021-12-31 09:30:51 +0100

    add spinner when loading game and switching game modes (#32)

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,9 @@
 import { createMainView } from '../components/mainView/mainView';
+import { spinner } from '../components/spinner/spinner';
 
 export const App = async ({ options }) => {
   const swquiz = document.getElementById('swquiz-app');
 
+  swquiz.appendChild(spinner());
   swquiz.appendChild(await createMainView());
 };
