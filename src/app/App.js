4bc6e93a9eb0621f86288b583933579bfa8029e9
Kos Krzysztof <krzysztof.kos33@gmail.com>
2022-01-01 20:32:58 +0100

    Removed console.log, updated game quiz view, remove package-lock.json from pr

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,9 @@
 import { createMainView } from '../components/mainView/mainView';
+import { createQuizGameView } from '../components/quizGameView/quizGameView';
 
 export const App = async ({ options }) => {
   const swquiz = document.getElementById('swquiz-app');
 
   swquiz.appendChild(await createMainView());
+  swquiz.appendChild(await createQuizGameView());
 };
--- src/components/quizGameView/quizGameView.js
+++ src/components/quizGameView/quizGameView.js
@@ -2,6 +2,7 @@ import './quizGameView.css';
 import createImgElementPeopleMode from '../recognitionImg/ImgModePeople/ImgModePeople';
 import { gameMode as createGameMode, getGameModeQuestion } from '../gameMode/gameMode';
 import { createAnswersCards} from '../answersCard/answersCard';
+import generateQuestionForTheGameMode from '../../services/game/generateQuestions';
 
 function createWrapperForComponent(className, nodeName) {
   if(typeof className !== 'string' && className.length < 2) throw new Error('An error occurred while create wrapper for components: argument className have to be a string type and have a length of at least 2');
@@ -13,9 +14,9 @@ function createWrapperForComponent(className, nodeName) {
   return componentWrapper;
 }
 
-export function createQuizGameView(question, gameMode) {
+export async function createQuizGameView(gameMode = 'people') {
+  const question = await generateQuestionForTheGameMode(gameMode);
   const { answers, rightAnswer, image } = question;
-  console.log(question);
 
   const quizGameView = document.createElement('main');
   quizGameView.classList.add('main-guiz-game');
