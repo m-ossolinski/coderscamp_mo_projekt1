b5de2624816aceafafdca8351ce1cf5ecd72575f
Kos Krzysztof <krzysztof.kos33@gmail.com>
2022-01-02 12:36:19 +0100

    fixed merge bug

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,6 @@
 import { createMainView } from '../components/mainView/mainView';
-<<<<<<< HEAD
 import { spinner } from '../components/spinner/spinner';
-=======
 import { createQuizGameView } from '../components/quizGameView/quizGameView';
->>>>>>> SWQ-35-quiz-game-mode
 
 export const App = async ({ options }) => {
   const swquiz = document.getElementById('swquiz-app');
